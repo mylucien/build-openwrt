a21e94675ab4a2dbbbef5ddfb173e184551ad7c0
mylucien <53183558+mylucien@users.noreply.github.com>
2024-08-29 21:52:31 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -12,3 +12,10 @@
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
+
+# 更新 `generic.mk` 文件
+sed -i '/define Device\/mercury_mw4530r-v1/s/\$(Device\/tplink-8mlzma)/\$(Device\/tplink-16mlzma)/' target/linux/ath79/image/generic.mk
+
+# 更新 `ar9344_tplink_tl-wdrxxxx.dtsi` 文件
+sed -i '91s/reg = <0x020000 0x7d0000>;$/reg = <0x020000 0xfd0000>;/' target/linux/ath79/dts/ar9344_tplink_tl-wdrxxxx.dtsi
+sed -i '96s/reg = <0x7f0000 0x010000>;$/reg = <0xff0000 0x010000>;/' target/linux/ath79/dts/ar9344_tplink_tl-wdrxxxx.dtsi
